99fe80dfd12cf3746f05316de691df3f1e369ada
Mikhail Supov <mikhailsupov@yandex.ru>
2024-10-05 00:34:29 +0300

    Update ExampleGnomeLinuxCornerMod.cs

--- Mods/Examples/ExampleGnomeLinuxCornerMod.cs
+++ Mods/Examples/ExampleGnomeLinuxCornerMod.cs
@@ -9,19 +9,19 @@ class ExampleGnomeLinuxCornerMod : LolibarMod
 {
     public override void PreInitialize()
     {
-        // Set it to true, to enable feature:
+        // Set it to true, to enable the feature:
         BarCornersInvokesDesktopsMenu = true;
 
         // Now you can simulate WIN+TAB key press,
-        // quick moving your mouse cursor to left corner of the statusbar.
+        // quick moving your mouse cursor to the Left corner of the statusbar.
 
-        // You can choose target corner:
+        // You can modify the target corner:
         BarTargetCorner = LolibarEnums.BarTargetCorner.Right;
 
-        // This value is default, so let's get back to it:
+        // This property equals Left by default, so let's get back to it:
         BarTargetCorner = LolibarEnums.BarTargetCorner.Left;
 
-        // So now, this feature compared to Left corner of the screen!
+        // So now, this feature compared to the Left corner of your screen!
 
         // But wait, which corner? Top Left? Bottom Left? Both???
 
