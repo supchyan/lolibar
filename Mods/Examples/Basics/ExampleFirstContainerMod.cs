b02e84108c8eae6eb512319ea2716403d294d330
Mikhail Supov <supchyan@yandex.ru>
2025-02-18 10:05:15 +0300

    Update ExampleFirstContainerMod.cs

--- Mods/Examples/Basics/ExampleFirstContainerMod.cs
+++ Mods/Examples/Basics/ExampleFirstContainerMod.cs
@@ -4,13 +4,13 @@ using System.Windows.Input;
 using System.Windows.Media;
 
 // This mod is outside of the Mods namespace, so it won't be loaded
-// You can uncomment namespace to make in loadable
+// You can uncomment namespace to enable (load) it
 
 //namespace LolibarApp.Mods;
 
 class ExampleFirstContainerMod : LolibarMod
 {
-    // This mod exmplains, how to create, customize and update containers.
+    // This mod explains, how to create, customize and update containers.
 
     // Let's define external Container type:
     LolibarContainer MyFirstContainer = new LolibarContainer();
@@ -22,35 +22,35 @@ class ExampleFirstContainerMod : LolibarMod
     }
     public override void Initialize()
     {
-        // Now let's setup our container with explanations:
+        // Now, let's setup our container with explanations:
 
 
-        // Container's name. You have to choose it,
-        // unless you won't overlap its resources with other containers.
+        // Container's name. You have to set it up,
+        // unless you want to overlap its resources with other containers.
         MyFirstContainer.Name = "MyCoolContainer";
 
 
-        // Text content. Won't be drawn, if you won't set it.
+        // Text content. Won't be drawn/shown, if you won't define it.
         MyFirstContainer.Text = "Hello world";
 
 
-        // Container's icon. Won't be drawn, if you won't set it.
+        // Container's icon. Won't be drawn/shown, if you won't define it.
         MyFirstContainer.Icon = Geometry.Empty;
 
-        // Let me exmplain, what Icon stores inside:
+        // Let me explain, what Icon stores inside:
         //
-        // Icon is `Geometry` class instance, which is sort of SVG icons representation.
-        // So, if you have a svg icon, you want to use as the container one, you have to do:
+        // So basically, Icon is `Geometry` class instance, which is sort of SVG icons representation.
+        // So, if you have a svg icon, and you want to use it in the container, you have to do:
         //
-        // 1. Get it's data. Just open svg icon file in text editor and copy its value.
-        // This will be long odd line of symbols, goes inside d="" but it's ok.
-        // That line is coordinates of icon's verticles, whitch uses to draw svg exactly.
+        // 1. Get it's `data`. Just open svg icon file in text editor and copy its value.
+        // This will be the long odd line of symbols in tag like d="".
+        // That tag is coordinates of icon's verticles, which are useful to draw a svg geometry exactly.
         //
-        // 2. Paste copied data to Icon property like:
+        // 2. Paste copied `data` into Icon property like:
         MyFirstContainer.Icon = Geometry.Parse("M16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM7 7.5C7 8.88071 5.88071 10 4.5 10C3.11929 10 2 8.88071 2 7.5C2 6.11929 3.11929 5 4.5 5C5.88071 5 7 6.11929 7 7.5ZM11.5 10C12.8807 10 14 8.88071 14 7.5C14 6.11929 12.8807 5 11.5 5C10.1193 5 9 6.11929 9 7.5C9 8.88071 10.1193 10 11.5 10Z");
 
-        // Geometry.Parse("") gets data string and tries to parse it as svg image.
-        // So, after pasting there your data, you have to see your icon inside the container!
+        // Geometry.Parse("") get a data string and then tries to parse it as svg image.
+        // So, after pasting your data in Parse(), you will see your icon inside the container!
         //
         // Important here. All default icons is relative to 16x16 grid in pixels,
         // so if you want to make a custom additional icon, draw it as svg in mentioned border restrictions.
