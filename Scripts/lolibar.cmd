7afcf32962f9ac79cb479a36ecc61fb5ff01f4f8
supchyan <mikhailsupov@yandex.ru>
2025-05-18 15:16:21 +0300

    `.cmd` improvements

--- Scripts/lolibar.cmd
+++ Scripts/lolibar.cmd
@@ -1,35 +1,26 @@
 @echo off
 
-if "%1"=="/h" (
-    call :HelpMessage
-    goto :eof
-)
 if "%1"=="/k" (
-    echo Killing lolibar...
+    echo Killing lolibar.exe...
     taskkill /IM lolibar.exe
     goto :eof
 )
 if "%1"=="/r" (
-    echo Restarting lolibar...
+    echo Restarting lolibar.exe...
     taskkill /IM lolibar.exe
     start lolibar.lnk
     goto :eof
 )
-if "%1"=="" (
-    echo Starting lolibar...
+if "%1"=="/s" (
+    echo Starting lolibar.exe...
     start lolibar.lnk
     goto :eof
 ) 
-if not "%1"=="/h" if not "%1"=="/k" if not "%1"=="/r" if not "%1"=="" (
-    call :HelpMessage
+if not "%1"=="/k" if not "%1"=="/r" if not "%1"=="/s" (
+    echo lolibar cli usage:
+    echo /h      - Shows this message,
+    echo /k      - Terminates all lolibar.exe processes,
+    echo /r      - Restarts lolibar.exe process,
+    echo /s      - Starts lolibar.exe process
     goto :eof
 )
-
-:HelpMessage
-echo [LOLIBAR CLI USAGE]
-echo /h     - Shows this message,
-echo /k     - Terminates all lolibar processes,
-echo /r     - Restarts lolibar process,
-echo -----------------------------------------------------------
-echo Calling [lolibar] with no flags starts lolibar.exe process.
-goto :eof
