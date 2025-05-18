46cc35a3497f6bda0c02b88586fc2250c7701656
supchyan <mikhailsupov@yandex.ru>
2025-05-18 15:33:35 +0300

    Better `.cmd`

--- Scripts/lolibar.cmd
+++ Scripts/lolibar.cmd
@@ -2,12 +2,12 @@
 
 if "%1"=="/k" (
     echo Killing lolibar.exe...
-    taskkill /IM lolibar.exe
+    call :KillLolibar
     goto :eof
 )
 if "%1"=="/r" (
     echo Restarting lolibar.exe...
-    taskkill /IM lolibar.exe
+    call :KillLolibar
     start lolibar.lnk
     goto :eof
 )
@@ -15,12 +15,19 @@ if "%1"=="/s" (
     echo Starting lolibar.exe...
     start lolibar.lnk
     goto :eof
-) 
-if not "%1"=="/k" if not "%1"=="/r" if not "%1"=="/s" (
-    echo lolibar cli usage:
-    echo /h      - Shows this message,
-    echo /k      - Terminates all lolibar.exe processes,
-    echo /r      - Restarts lolibar.exe process,
-    echo /s      - Starts lolibar.exe process
+)
+
+echo lolibar cli usage:
+echo /h      - Shows this message,
+echo /k      - Terminates all lolibar.exe processes,
+echo /r      - Restarts lolibar.exe process,
+echo /s      - Starts lolibar.exe process
+goto :eof
+
+:KillLolibar
+:loop
+taskkill /im "lolibar.exe" >NUL 2>&1 && (
+    goto :loop
+) || (
     goto :eof
 )
