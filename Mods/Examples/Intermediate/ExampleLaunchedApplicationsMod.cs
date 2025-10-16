5262c09105a1747463cdef401e85ffeb6a7324ae
Mikhail Supov <supchyan@yandex.ru>
2025-10-16 15:50:33 +0000

    Update ExampleLaunchedApplicationsMod.cs

--- Mods/Examples/Intermediate/ExampleLaunchedApplicationsMod.cs
+++ Mods/Examples/Intermediate/ExampleLaunchedApplicationsMod.cs
@@ -1,6 +1,9 @@
 ﻿using LolibarApp.Source.Tools;
 using LolibarApp.Source;
 
+// This mod is outside of the Mods namespace, so it won't be loaded
+// You can uncomment namespace to enable (load) it
+
 //namespace LolibarApp.Mods;
 
 class ExampleLaunchedApplicationsMod : LolibarMod
